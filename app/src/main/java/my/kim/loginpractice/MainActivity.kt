691feccd8cbd2bbcba0912d091693da23a951f7e
mykim76 <iyoungfm@gmail.com>
2020-05-27 20:51:55 +0900

    로그인 체크기능 구현 => 조건문, 비교연산, 논리연산

--- app/src/main/java/my/kim/loginpractice/MainActivity.kt
+++ app/src/main/java/my/kim/loginpractice/MainActivity.kt
@@ -11,6 +11,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        loginBtn.setOnClickListener { 
+            val inputEmail = emailEdt.text.toString()
+            val inputPW = pwEdt.text.toString()
+            
+            //email과 pw 확인하여 같거나 다른 경우 처리가 다름 조건문 사용법 익히기
+            if(inputEmail=="admin@test.com" && inputPW =="qwer")
+            {
+                Log.d("로그인 확인","관리자 맞음")
+                Toast.makeText(this,"관리자입니다",Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                Log.d("로그린 확인","관리자 아님")
+
+                Toast.makeText(this,"로그인샐패했습니다.",Toast.LENGTH_SHORT).show()
+            }
+            
+        }
 
         eventBtn.setOnClickListener{
 
